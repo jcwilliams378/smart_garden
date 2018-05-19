3ebbc33afd047cc0c979088945bf3c2eeebdd1b9
Jeffrey Williams <jcwilliams378@gmail.com>
2018-05-19 17:33:23 -0400

    added in usb HD logging

--- garden.py
+++ garden.py
@@ -4,6 +4,7 @@ import DS18B20_air_temp
 import camera_module
 import water_flow_sensor
 import BH1750FVI_lux_meter
+import csv
 
 from Adafruit_IO import Client, MQTTClient
 import datetime
@@ -38,8 +39,15 @@ water_flag = False
 valve_status_prev = False
 moisture_threshold = 100
 half_water_dwell_time = 5
+lux = 0
 
 i_o_dict = {'ON':GPIO.HIGH,'OFF':GPIO.LOW, True:'ON',False:'OFF'}
+
+f_name = "/mnt/volume/garden_" + time.strftime("%Y-%m-%d %H_%M_%S") + ".csv"
+
+with open(f_name, 'wb') as outcsv:
+    writer = csv.writer(outcsv, quoting=csv.QUOTE_ALL)
+    writer.writerow(["Time","Air Temp","Soil Temp","Soil Moisture","Light Illuminance","Water Flow","Valve Status"])
 
 def send_to_AIO(stream__name, value):
     try:
@@ -63,14 +71,16 @@ while(aio.receive('web_kill_switch').value=='ALIVE!' and not physical_kill_switc
 
         send_to_AIO('camera_feed',img)
 
-        lux = lux_meter.readLight()
-        send_to_AIO('light_lux',lux)
-
         image_clock = time.time()
 
     # see if the soil is dry enough after 9PM to see if the water should be on:
     t_now = datetime.datetime.now()
-    if t_now.hour == 21 and t_now.now().minute <= 30 and not auto_water_flag and int(aio.receive('soil_moisture').value) < moisture_threshold:
+    moisture = aio.receive('soil_moisture').value
+    soil_temp = aio.receive('soil_temp').value
+    lux = lux_meter.readLight()
+    send_to_AIO('light_lux',lux)
+
+    if t_now.hour == 21 and t_now.now().minute <= 30 and not auto_water_flag and int(moisture) < moisture_threshold:
         auto_water_flag = True
         water_timer = time.time()
     else:
@@ -105,6 +115,10 @@ while(aio.receive('web_kill_switch').value=='ALIVE!' and not physical_kill_switc
 
     elapsed_time = time.time() - loop_start_time
     print(elapsed_time)
+
+    with open(f_name, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow([time.strftime("%Y-%m-%d %H:%M:%S"), str(air_temp), str(soil_temp), str(moisture), str(lux), str(flow), str(i_o_dict[valve_status])])
 
     while elapsed_time < short_cycle_time:
         if GPIO.input(PI_RESET_PIN_IN):
